fa034252be727506927ee3c299213d30457ae6f0
hugopage17 <43106346+hugopage17@users.noreply.github.com>
2018-09-10 19:33:21 +1200

    Add files via upload

---
+++ Armstrong.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+
+ /*Please dont change class name, Dcoder 
+ and class must not be public*/
+
+ //Compiler version JDK 1.8
+
+
+ class Dcoder
+ { 
+	public static void main(String args[])
+   { 
+   	int c=0,a,temp;
+   	int n=153;
+   	temp=n;
+   	while(n>0)
+   	{
+   		a=n%10;
+   		n=n/10;
+   		c=c+(a*a*a);
+   	}
+   	if(temp==c)
+   	System.out.println("Armstrong number");
+   	else
+   	System.out.println("Not Armstrong");
+
+ 	}	
+ }
